ffd2b50bd65e1ea9fec034fce11be08c13b24d45
Michael Zhang <mzhang@emergent360.com>
2025-06-24 10:11:44 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import '@patternfly/react-core/dist/styles/base.css';
+import * as React from 'react';
+import {
+  Page,
+  PageSidebar,
+  PageSection,
+  Nav,
+  NavItem,
+  NavList,
+  Masthead,
+  MastheadMain,
+  MastheadBrand,
+  MastheadToggle
+} from '@patternfly/react-core';
+
 function App() {
+  const header = (
+    <Masthead>
+      <MastheadToggle />
+      <MastheadMain>
+        <MastheadBrand>My App</MastheadBrand>
+      </MastheadMain>
+    </Masthead>
+  );
+
+  const nav = (
+    <Nav>
+      <NavList>
+        <NavItem itemId={0} isActive>Home</NavItem>
+        <NavItem itemId={1}>About</NavItem>
+      </NavList>
+    </Nav>
+  );
+
+  const sidebar = <PageSidebar nav={nav} isNavOpen />;
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello, OpenShift!</h1>
-    </div>
+    <Page header={header} sidebar={sidebar}>
+      <PageSection>
+        <h1>Hello World - Now using PatternFly</h1>
+      </PageSection>
+    </Page>
   );
 }
 
